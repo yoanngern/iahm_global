008598cab460c29cfbef9eaae671d9bddcfdf787
yoanngern <yoann@yoanngern.ch>
2016-03-10 20:53:29 +0100

    change routing

--- web/config.php
+++ web/config.php
@@ -17,6 +17,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '84.253.11.167',
+    '85.7.250.13',
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
