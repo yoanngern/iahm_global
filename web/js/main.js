90b8f16642c2ab0730d6e6077fa7c3dac6d93bce
yoanngern <yoann@yoanngern.ch>
2016-04-06 12:06:53 +0200

    countdown auto

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,5 @@
 // @codekit-prepend "vendor/jquery-2.2.2.js"
+// @codekit-prepend "vendor/jquery.countdown.js"
 
 $(document).ready(function () {
 
@@ -6,5 +7,50 @@ $(document).ready(function () {
         event.preventDefault();
         $("body > header ul#mobile").toggleClass("show");
     });
+
+
+    $end_time = $('#countdown').data("time");
+    $language = $('#countdown').data("lang");
+
+    $('#countdown').countdown($end_time, function (event) {
+
+        if (event.type == "stoped") {
+            $('#countdown').attr("data-live", "on");
+        }
+
+        if ($language == "fr") {
+            $value = "LIVE dans ";
+            $day = "jour";
+            $days = "jours";
+        } else {
+            $value = "LIVE in ";
+            $day = "day";
+            $days = "days";
+        }
+
+        if (event.offset.days > 1) {
+
+            $value += event.strftime('%-d ');
+            $value += $days;
+
+
+        } else if (event.offset.days == 1) {
+
+            $value += event.strftime('%-d ');
+            $value += $day;
+
+        } else if (event.offset.hours >= 1) {
+
+            $value += event.strftime('%-Hh');
+
+        } else {
+
+            $value += event.strftime('%-M\' %S\'\'');
+        }
+
+
+        $('.countdown_txt', this).html($value);
+    });
+
 
 });
