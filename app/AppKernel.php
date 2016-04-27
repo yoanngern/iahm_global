d817e58e5b4d3a219804eeeae64c43632612816f
yoanngern <yoann@yoanngern.ch>
2016-04-27 14:09:04 +0200

    add privilege

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new iahmLiveBundle\iahmLiveBundle(),
+            new iahmEventBundle\iahmEventBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
